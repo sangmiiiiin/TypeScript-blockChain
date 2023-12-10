b78354d3e9925064081e1af2f8c63278ab103931
SANGMIN <kwk680037@gmail.com>
2023-12-10 20:54:11 +0900

    모듈을 사용할때 타입스크립트에게 타입정의 하는법

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,10 +1,7 @@
+import { init, exit } from "myPackage";
 
+init({
+    url:"true"
+})
 
-class Block {
-    constructor(
-        private data: string
-    ) {}
-    static hello() {
-        return "hi";
-    }
-}
+exit(1)
