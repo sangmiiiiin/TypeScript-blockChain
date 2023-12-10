031f646ff32e336760c4e1ba73100b039faf694f
SANGMIN <kwk680037@gmail.com>
2023-12-11 00:03:58 +0900

    Npm에 있는 모듈 타입 정의 불러오는 방법

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,4 +1,25 @@
-import { init, exit } from "myPackage";
+import crypto, { Hash } from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;    
+}
 
 
-init()
+
+class Block implements BlockShape{
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
